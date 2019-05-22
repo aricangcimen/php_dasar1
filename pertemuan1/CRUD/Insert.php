11540c9584a627af497051bcdd2dfa97fe7127d9
aricangcimen <antosan1234567@gmail.com>
2019-05-23 02:16:47 +0700

    menambah crud

---
+++ pertemuan1/CRUD/Insert.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Simple CRUD - Create </title>
+</head>
+<body>
+    <h1>Create Product </h1>
+    <table>
+    <tr>
+    <td>Product Name</td>
+    <td>:</td>
+    <td><input type="text" name = "productName"></td>
+    </tr>
+    <tr>
+    <td>Product Description </td>
+    <td>:</td>
+    <td><textarea name = "product Description"></textarea></td>
+    </tr>
+    <tr>
+    <td>Product Price</td>
+    <td>:</td>
+    <td><input type="number" name = "product price"></td>
+    </tr>
+    </table>
+</body>
+</html>
---
+++ pertemuan1/CRUD/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CRUD Sederhana</title>
+</head>
+<body>
+<h1> Simple CRUD </h1>
+    <a href ="Insert.php"> Insert </a>
+    <a href ="Update.php"> Update </a>
+    <a href ="Delet.php"> Delet </a>
+    <a href ="View.php" > View </a>
+</body>
+</html>
