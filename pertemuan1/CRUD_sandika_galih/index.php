04af9cea7a1b6675bb82720782240570649a8f53
aricangcimen <antosan1234567@gmail.com>
2019-05-23 19:25:36 +0700

    coba lagi

--- pertemuan1/CRUD_sandika_galih/index.php
+++ pertemuan1/CRUD_sandika_galih/index.php
@@ -5,10 +5,17 @@ var_dump($result);
 if(!result){
 echo mysqli_error($conn);
 // memunculkan errornya
-}
-?>
+//ambil data (fetch) mahasiswa dari object result
+//mysqli_fecth_row() mengembalikan arary numeric
+//mysqli_fetch_assoc()
+//mysqli_fetch_array()
+//mysqli_fetch_object()
 
-
+$mhs = mysqli_fetch_row($result);
+var_dump($mhs);
+$mhs1 = mysqli_fetch_assoc($result);
+var_dump($mhs1["jurusan"]);
+$mhs2
 
 <!DOCTYPE html>
 <html lang="en">
