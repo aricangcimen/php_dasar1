e36fe6d199655dd20f5d8bb4fa15a5c3fcbea7b8
aricangcimen <antosan1234567@gmail.com>
2019-05-23 19:42:59 +0700

    situasi yuk

--- pertemuan1/CRUD_sandika_galih/index.php
+++ pertemuan1/CRUD_sandika_galih/index.php
@@ -7,16 +7,18 @@ echo mysqli_error($conn);
 // memunculkan errornya
 //ambil data (fetch) mahasiswa dari object result
 //mysqli_fecth_row() mengembalikan arary numeric
-//mysqli_fetch_assoc()
-//mysqli_fetch_array()
+//mysqli_fetch_assoc()mengembalikan arary assoc
+//mysqli_fetch_array()mengembalikan arrary numeric dan assoc
 //mysqli_fetch_object()
 
 $mhs = mysqli_fetch_row($result);
 var_dump($mhs);
 $mhs1 = mysqli_fetch_assoc($result);
 var_dump($mhs1["jurusan"]);
-$mhs2
-
+$mhs2 = mysqli_fetch_array($result);
+var_dump($mhs2[4]);
+$mhs3 = mysqli_fetch_object($result);
+var_dump($mhs -> email);
 <!DOCTYPE html>
 <html lang="en">
 <head>
