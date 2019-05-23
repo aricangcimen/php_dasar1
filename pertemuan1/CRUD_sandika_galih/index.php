bfc0376fada982ecce375d6a77bdecd4a6c27cd3
aricangcimen <antosan1234567@gmail.com>
2019-05-23 07:25:52 +0700

    konnekt database

---
+++ pertemuan1/CRUD_sandika_galih/index.php
@@ -0,0 +1,21 @@
+<?php
+$conn = mysqli_connect("localHost","root", "root","phpdasar");
+$result = mysqli_query($conn , "SELECT * FROM mahasiswa");
+var_dump($result);
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
